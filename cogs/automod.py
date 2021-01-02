d0014434a27132c7f6a1a34efd90b47ad34d706c
TimPidashev <pidashev.tim@gmail.com>
2021-01-02 13:55:02 -0800

    Added auto-moderation(bad words)

---
+++ cogs/automod.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+class AutoMod(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    #on_ready
+    async def on_ready(self):
+        print("cog AutoMod online...")
+
+    #bad words
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        bad_words = ["fick", "arsch", "Arschgesicht", "arschgesicht", "Arschloch", "Asshole", "asshole", "Fotze", "fotze", "Miststück", "miststück", "Bitch", "bitch", "Schlampe", "schlampe", "Sheisse", "sheisse",  "Shit", "shit", "Fick", "huren", "Verpiss", "verpiss", "masturbiert", "Idiot", "idiot", "depp", "Depp", "Dumm", "dumm", "jude", "Bastard", "bastard", "Wichser", "wichser", "wixxer", "Wixxer", "Hurensohn" "Wixer", "Pisser", "Arschgesicht", "huso", "hure", "Hure", "verreck" "Verreck", "fehlgeburt", "Fehlgeburt", "ficken", "adhs", "ADHS", "Btch", "faggot", "fck", "f4ck", "nigga", "Nutted", "flaschengeburt", "penis", "pusse", "pusse", "pussy", "pussys", "nigger", "kacke", "fuucker", "fuck"]
+        for word in bad_words:
+            if message.content.count(word) > 0:
+                await message.channel.purge(limit=1)
+                await message.channel.send(f"Cussing is not allowed! {message.author.mention}")
+
+def setup(client):
+    client.add_cog(AutoMod(client))
