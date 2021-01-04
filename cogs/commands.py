5a9df22241a9a3c02bc8f0fd3b1ab4c7104eac81
TimPidashev <pidashev.tim@gmail.com>
2021-01-03 17:22:07 -0800

    removed default ping command

--- cogs/commands.py
+++ cogs/commands.py
@@ -93,16 +93,6 @@ class Commands(commands.Cog):
                 await message.delete()
                 break
 
-    #ping
-    @commands.command()
-    async def ping(self, context):
-        """ Pong! """
-        before = time.monotonic()
-        before_ws = int(round(self.bot.latency * 1000, 1))
-        message = await context.send("🏓 Pong")
-        ping = (time.monotonic() - before) * 1000
-        await message.edit(content=f"🏓 WS: {before_ws}ms  |  REST: {int(ping)}ms")
-
     #version
     @commands.command()
     async def version(self, context):
