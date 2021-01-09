1c573bb705fcf8cedef37be38ce390e747a7ffd6
TimPidashev <pidashev.tim@gmail.com>
2021-01-09 13:30:34 -0800

    more music fixes

--- cogs/music.py
+++ cogs/music.py
@@ -1,7 +1,15 @@
 import discord
 import youtube_dl
+import os
 from discord.ext import commands
-
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
 class music(commands.Cog):
     def __init__(self, client):
         self.bot = client
