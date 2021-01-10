3e0475ce28013c8865ae5c24364999feb3788ca5
TimPidashev <pidashev.tim@gmail.com>
2021-01-09 20:30:30 -0800

    redid level cog

--- cogs/level.py
+++ cogs/level.py
@@ -1,31 +1,13 @@
 import discord
 import json
-import asyncio
-import time
 import random
+import os
 from discord.ext import commands
 
 class level(commands.Cog):
     def __init__(self, client):
         self.bot = client
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("cog level online...")
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        with open("users.json", "r") as f:
-            users = json.load(f)
-
-        await update_data(users, member)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-
-    async def update_data(self, users, user):
-        if not user.id in users:
-            users[user.id] - {}
 
 def setup(client):
     client.add_cog(level(client))
