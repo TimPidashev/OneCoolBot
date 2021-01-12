621c3e3b3a9a360212772e73833ca734358e5677
TimPidashev <pidashev.tim@gmail.com>
2021-01-12 10:18:43 -0800

    Reverted music cog from youtube_dl to wavelink

--- cogs/music.py
+++ cogs/music.py
@@ -1,30 +1,16 @@
 import discord
-import youtube_dl
+import wavelink
 import os
+import asyncio
 from discord.ext import commands
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
+
 class music(commands.Cog):
     def __init__(self, client):
-        self.bot = client
+        self.bot = self.client
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("cog music online...")
 
-    @commands.command()
-    async def play(self, context):
-        if not context.message.author.voice:
-            await context.send("You are not connected to a voice channel.")
-        else:
-            channel = context.message.author.voice.channel
-            await channel.connect()
+
+
 
 
 def setup(client):
