d2a146b5a78d9e955ea4cbc10d28c50d11ebb079
TimPidashev <pidashev.tim@gmail.com>
2021-01-21 12:18:23 -0800

    added script to dms, but is broken at the moment

--- cogs/dms.py
+++ cogs/dms.py
@@ -2,8 +2,10 @@ import discord
 import os
 import asyncio
 import time
+from discord import DMChannel
 from discord.ext import commands
 from termcolor import colored
+from datetime import datetime
 
 class dms(commands.Cog):
     def __init__(self, client):
@@ -13,9 +15,36 @@ class dms(commands.Cog):
     async def on_ready(self):
         print(colored("[dms]: cog dms online...", "grey"))
 
-    @commands.command()
-    async def chat(self, context):
-        await context.channel.send(f"{context.author}, my A.I. is not yet functional, go bug Timmy to update me!")
-        print(f"[dms]: {context.author} is bugging you to update me!", "grey")
+    #currently broken
+    
+    #@commands.command()
+    #async def report(self, message):
+        #if not message.author.bot:
+            #if isinstance(message.channel, DMChannel):
+                #if len(message.content) < 50:
+                    #await context.send("Your message should be at leas 50 characters in length.")
+                #else:
+                    #await context.send("Your message has been accepted...")
+
+
+        #else:
+            #await self.process_commands(message)
+
+
+
+
+
+    #modmail system in the works
+    #@commands.Cog.listener()
+    #async def on_message(self, message):
+        #if not message.author.bot:
+            #if isinstance(message.channel, DMChannel):
+                #if len(message.content) < 50:
+                    #await context.send("Your message should be at leas 50 characters in length.")
+
+        #else:
+            #await self.process commands(message)
+
+
 def setup(client):
     client.add_cog(dms(client))
