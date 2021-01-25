2e833bd67db3ba09654d233e19dec5ee719f739a
TimPidashev <pidashev.tim@gmail.com>
2021-01-25 13:23:42 -0800

    simple api script

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -8,6 +8,28 @@ class crypto(commands.Cog):
     def __init__(self, client):
         self.bot = client
 
+    @commands.guild_only()
+    @commands.command()
+    async def crypto(self, context):
+        headers = {
+            'X-CMC_PRO_API_KEY' : '8f8259ec-f70f-4fca-aff8-f6638d6aa614',
+            'Accepts' : 'applications/json'
+        }
+
+        params = {
+            'start' : '1',
+            'limit' : '5',
+            'convert' : 'USD'
+        }
+
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+        json = requests.get(url, params=params, headers=headers).json()
+
+        coins = json['data']
+
+        for x in coins:
+            print(x['symbol'], x['quote']['USD']['price'])
 
 def setup(client):
     client.add_cog(crypto(client))
