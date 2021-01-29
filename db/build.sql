fd438b0bbfc3f6dbae92194b0bf4b70b5c04b4d3
TimPidashev <pidashev.tim@gmail.com>
2021-01-29 11:44:48 -0800

    added build.sql

---
+++ db/build.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS guilds(
+    GuildID integer PRIMARY KEY,
+    Prefix text DEFAULT "d!",
+    LogChannel text,
+    MutedRole text,
+    StarBoardChannel text,
+    LevelMessages text DEFAULT "no"
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    UserID integer PRIMARY KEY,
+    XP integer DEFAULT 0,
+    Level integer DEFAULT 0,
+    XPLock text DEFAULT CURRENT_TIMESTAMP,
+    OverwatchUsername text,
+    OverwatchPlatform text,
+    OverwatchRegion text,
+    LastfmUsername text,
+    osuUsername text
+);
+
+CREATE TABLE IF NOT EXISTS "mutes" (
+	UserID integer,
+	GuildID integer,
+	RoleIDs	text,
+	PRIMARY KEY("UserID","GuildID")
+);
+
+CREATE TABLE IF NOT EXISTS votes(
+	UserID integer PRIMARY KEY,
+	HAVEVOTED text DEFAULT "no",
+	VoteLock text DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS guildexp(
+    GuildID integer,
+    UserID integer,
+    XP integer DEFAULT 0,
+    Level integer DEFAULT 0,
+    XPLock text DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("GuildID", "UserID")
+);
+
+CREATE TABLE IF NOT EXISTS starboard(
+    MessageID integer,
+    StarMessageID integer,
+    Stars integer DEFAULT 1,
+    GuildID integer,
+    PRIMARY KEY("MessageID", "GuildID")
+)
