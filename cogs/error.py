0f88c9323d25977a5812ce51e3e9846f67b73244
timothypidashev <pidashev.tim@gmail.com>
2021-04-01 02:49:54 +0000

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -14,32 +14,32 @@ class errorhandler(commands.Cog):
     async def on_ready(self):
         print(colored("[error]: online...", "red"))
 
-    # @commands.Cog.listener()
-    # async def on_command_error(self, context, error):
-    #     #gets original error
-    #     error = getattr(error, 'original', error)
-    #
-    #     #if a local error handler exists...
-    #     if hasattr(context.command, 'on_error'):
-    #         return
-    #
-    #     #command_not_found
-    #     if isinstance(error, commands.CommandNotFound):
-    #         print(colored("[error]: An invalid command was sent, ignoring...", "red"))
-    #         return
-    #
-    #     #disabled_command
-    #     if isinstance(error, commands.DisabledCommand):
-    #         print(colored("[error]: A disabled command was sent, ignoring...", "red"))
-    #         return
-    #
-    #     #missing_permissions
-    #     if isinstance(error, commands.MissingPermissions):
-    #         print(colored("[error]: User was missing permissions, ignoring...", "red"))
-    #         return
-    #
-    #     else:
-    #         pass
+    @commands.Cog.listener()
+    async def on_command_error(self, context, error):
+        #gets original error
+        error = getattr(error, 'original', error)
+    
+        #if a local error handler exists...
+        if hasattr(context.command, 'on_error'):
+            return
+    
+        #command_not_found
+        if isinstance(error, commands.CommandNotFound):
+            print(colored("[error]: An invalid command was sent, ignoring...", "red"))
+            return
+    
+        #disabled_command
+        if isinstance(error, commands.DisabledCommand):
+            print(colored("[error]: A disabled command was sent, ignoring...", "red"))
+            return
+    
+        #missing_permissions
+        if isinstance(error, commands.MissingPermissions):
+            print(colored("[error]: User was missing permissions, ignoring...", "red"))
+            return
+    
+        else:
+            pass
 
 def setup(client):
     client.add_cog(errorhandler(client))
