b0037e57a5f20d2e56a1efea55d7deb940961b0b
timothypidashev <pidashev.tim@gmail.com>
2021-04-09 19:27:25 +0000

    more level changes

--- cogs/level.py
+++ cogs/level.py
@@ -69,42 +69,44 @@ class level(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
-            result = db.record("SELECT UserID FROM users WHERE UserID = (?)", message.author.id)
-            if result is not None:
-                xp, lvl, xplock = db.record("SELECT XP, Level, XPLock FROM users WHERE UserID = ?", message.author.id)
-                if datetime.utcnow() > datetime.fromisoformat(xplock):
+            guild_id, user_id = db.record("SELECT GuildID, UserID FROM users WHERE (GuildID, UserId) = (?, ?)",
+                message.guild.id,
+                message.author.id
+            )
+            if guild_id is not None:
+                print("success")
+                # xp, lvl, xplock = db.record("SELECT XP, Level, XPLock FROM users WHERE UserID = ?", message.author.id)
+                # if datetime.utcnow() > datetime.fromisoformat(xplock):
 
-                    xp_to_add = random.randint(10, 20)
-                    new_lvl = int(((xp + xp_to_add) // 42) ** 0.55)
-                    coins_on_xp = random.randint(1, 10)
-                    db.execute(f"UPDATE users SET XP = XP + ?, Level = ?, Coins = Coins + ?, XPLock = ? WHERE UserID = {message.author.id} AND GuildID = {message.guild.id}",
-                        xp_to_add,
-                        new_lvl,
-                        coins_on_xp,
-                        (datetime.utcnow() + timedelta(seconds=50)).isoformat(),
-                    )
+                #     xp_to_add = random.randint(10, 20)
+                #     new_lvl = int(((xp + xp_to_add) // 42) ** 0.55)
+                #     coins_on_xp = random.randint(1, 10)
+                #     db.execute(f"UPDATE users SET XP = XP + ?, Level = ?, Coins = Coins + ?, XPLock = ? WHERE UserID = {message.author.id} AND GuildID = {message.guild.id}",
+                #         xp_to_add,
+                #         new_lvl,
+                #         coins_on_xp,
+                #         (datetime.utcnow() + timedelta(seconds=50)).isoformat(),
+                #     )
 
-                    db.commit()
-                    print(colored("[level]:", "magenta"), colored(f"Added {xp_to_add} xp to {message.author}...", "cyan"))
-                    print(colored("[economy]:", "magenta"), colored(f"Added {coins_on_xp} coins to {message.author}...", "blue"))
+                #     db.commit()
+                #     print(colored("[level]:", "magenta"), colored(f"Added {xp_to_add} xp to {message.author}...", "cyan"))
+                #     print(colored("[economy]:", "magenta"), colored(f"Added {coins_on_xp} coins to {message.author}...", "blue"))
 
-                    if new_lvl > lvl:
-                        await message.channel.send(f":partying_face: {message.author.mention} is now level **{new_lvl:,}**!")
-                        print(colored("[level]:", "magenta"), colored(f"{message.author} has leveled up to {new_lvl:,}...", "cyan"))
+                #     if new_lvl > lvl:
+                #         await message.channel.send(f":partying_face: {message.author.mention} is now level **{new_lvl:,}**!")
+                #         print(colored("[level]:", "magenta"), colored(f"{message.author} has leveled up to {new_lvl:,}...", "cyan"))
 
-                else:
-                    pass
+                # else:
+                #     pass
 
             else:
-
-                """FIX THIS TOMORROW ASAP"""
-
-                # db.execute("INSERT OR IGNORE INTO users SELECT (GuildID) VALUE (?)", message.guild.id)
-                # db.commit()
-                # db.execute("INSERT OR IGNORE INTO users SELECT (UserID) VALUE (?)", message.author.id)
-                # db.commit()
-
-                print(colored("[level]:", "magenta"), colored(f"{message.author}#{message.author.discriminator} was added to level db...", "cyan"))
+                
+                db.execute("INSERT OR IGNORE INTO users (GuildID, UserID) VALUES (?, ?)",
+                    message.guild.id,
+                    message.author.id
+                )
+                db.commit()
+                print(colored("[level]:", "magenta"), colored(f"{message.author}#{message.author.discriminator} in guild: {message.guild.name} was added to users table...", "cyan"))
 
 
     @commands.command()
