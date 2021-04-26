1f8858e9ed07bc908a69c0d4979a0a76eb52d6f0
timothypidashev <pidashev.tim@gmail.com>
2021-04-26 17:13:19 +0000

    quick and dirty moderation rewrite

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -23,51 +23,51 @@ class moderation(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def clear(self, context, amount=10):
         await context.channel.purge(limit=amount+1)
-        print(colored(f"[moderation]: removed {amount} messages...", "yellow"))
+        print(colored("[moderation]:", "magenta"), colored(f"removed {amount} messages...", "yellow"))
 
     #kick
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, context, member: discord.Member, *, reason=None):
-        kickServerEmbed = discord.Embed(
+        embed1 = discord.Embed(
             colour = discord.Colour.red(),
             title = f"{context.author} kicked {member.name}!",
             description = f"**Reason:** {reason}\n**By:** {context.author.mention}",
         )
-        kickPrivateEmbed = discord.Embed(
+        embed2 = discord.Embed(
             colour = discord.Colour.red(),
             title = f"Oh no! You were kicked by {context.author}!",
             description = f"**Reason:** {reason}\n"
         )
         try:
-            await context.channel.send(embed=kickServerEmbed)
-            await member.send(embed=kickPrivateEmbed)
+            await context.channel.send(embed=embed1)
+            await member.send(embed=embed2)
             await member.kick(reason=reason)
-            print(colored(f"[moderation]: {context.author} kicked {member}...", "yellow"))
+            print(colored("[moderation]:", "magenta"), colored(f"{context.author} kicked {member}...", "yellow"))
         except:
-            print(colored(f"[moderation]: an error occured while {context.author} was trying to kick/kicked {member}...", "red"))
+            print(colored("[moderation]:", "magenta"), colored(f"an error occured while {context.author} was trying to kick/kicked {member}...", "red"))
 
     #ban
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, context, member: discord.Member, *, reason=None):
-       banServerEmbed = discord.Embed(
+       embed1 = discord.Embed(
             colour = discord.Colour.red(),
             title = f"{context.author} banned {member.name}!",
             description = f"**Reason:** {reason}\n**By:** {context.author.mention}",
        )
-       banPrivateEmbed = discord.Embed(
+       embed2 = discord.Embed(
             colour = discord.Colour.red(),
             title = f"Oh no! You were banned by {context.author}!",
             description = f"**Reason:** {reason}\n"
        )
        try:
-           await context.channel.send(embed=banServerEmbed)
-           await member.send(embed=banPrivateEmbed)
+           await context.channel.send(embed=embed1)
+           await member.send(embed=embed2)
            await member.ban(reason=reason)
-           print(colored(f"[moderation]: {context.author} banned {member}...", "yellow"))
+           print(colored("[moderation]:", "magenta"), colored(f"{context.author} banned {member}...", "yellow"))
        except:
-            print(colored(f"[moderation]: an error occured while {context.author} was trying to ban/banned {member}...", "red"))
+            print(colored("[moderation]:", "magenta"), colored(f"an error occured while {context.author} was trying to ban/banned {member}...", "red"))
 
     #unban(works, but needs help)
     @commands.command()
@@ -79,16 +79,16 @@ class moderation(commands.Cog):
             user = ban_entry.user
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await context.guild.unban(user)
-                unbanServerEmbed = discord.Embed(
+                embed = discord.Embed(
                     colour = discord.Colour.green(),
                     title = f"{context.author} unbanned {user.name}#{user.discriminator}!",
                     description = f"**Reason:** Good Behavior **By:** {context.author.mention}",
                 )
-                await context.channel.send(embed=unbanServerEmbed)
-                print(colored(f"[moderation]:  {context.author} unbanned {user.name}#{user.discriminator}...", "yellow"))
+                await context.channel.send(embed=unban)
+                print(colored("[moderation]:", colored(f"{context.author} unbanned {user.name}#{user.discriminator}...", "yellow"))
                 return
             else:
-                print(colored(f"[moderation]: {context.author} tried to unban/unbanned {user.name}#{user.discriminator}, but internal error occured...", "red"))
+                print(colored("[moderation]:", colored(f"{context.author} tried to unban/unbanned {user.name}#{user.discriminator}, but internal error occured...", "red"))
 
     #automoderation section below
     @commands.Cog.listener()
@@ -103,7 +103,7 @@ class moderation(commands.Cog):
                     description = "Discord invite links are not allowed!"
                 )
                 await message.author.send(embed=embed)
-                print(colored(f"[moderation]: {message.author} tried to advertise: {message.content}...", "yellow"))
+                print(colored("[moderation]:", "magenta"), colored(f"{message.author} tried to advertise: {message.content}...", "yellow"))
 
             elif profanity.contains_profanity(message.content):
                 await message.delete()
@@ -113,7 +113,7 @@ class moderation(commands.Cog):
                     description = "Profanity is not allowed on this server!"
                 )
                 await message.author.send(embed=embed)
-                print(colored(f"[moderation]: {message.author}'s message({message.content}) was deleted...", "yellow"))
+                print(colored("[moderation]:", "magenta"), colored(f"{message.author}'s message({message.content}) was deleted...", "yellow"))
 
         else:
             pass
