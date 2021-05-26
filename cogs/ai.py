ede2c3f0ede3ada6b056b8c736b93d6b3aef3a33
timothypidashev <pidashev.tim@gmail.com>
2021-05-26 21:00:19 +0000

    Update dataset and chatmodel functions + polish

--- cogs/ai.py
+++ cogs/ai.py
@@ -12,14 +12,6 @@ import asyncio
 
 test_channel = 846428895981076510
 
-with open('./ai/tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-
-with open('./ai/label_encoder.pickle', 'rb') as enc:
-    lbl_encoder = pickle.load(enc)
-
-max_len = 20
-
 class ai(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -31,21 +23,34 @@ class ai(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
-            if message.channel.id == test_channel:
-                with open("./ai/intents/intents.json") as file:
-                    data = json.load(file)
-                    model = keras.models.load_model('./ai/chat_model')
-                    inp = message.content
-                    result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]), truncating='post', maxlen=max_len))
-                    tag = lbl_encoder.inverse_transform([np.argmax(result)])
+            context = await self.client.get_context(message)
+            if context.command:
+                return
+            if not self.client.user.mentioned_in(message):
+                return
 
-                    for i in data['intents']:
-                        if i['tag'] == tag:
-                            response = np.random.choice(i['responses'])
-                            
-                            async with message.channel.typing():
-                                await asyncio.sleep(1)
-                                await message.reply(response, mention_author=False)
+            with open('./ai/tokenizer.pickle', 'rb') as handle:
+                tokenizer = pickle.load(handle)
+
+            with open('./ai/label_encoder.pickle', 'rb') as enc:
+                lbl_encoder = pickle.load(enc)
+
+            max_len = 20
+
+            with open("./ai/intents/intents.json") as file:
+                data = json.load(file)
+                model = keras.models.load_model('./ai/chat_model')
+                inp = message.content
+                result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]), truncating='post', maxlen=max_len))
+                tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+                for i in data['intents']:
+                    if i['tag'] == tag:
+                        response = np.random.choice(i['responses'])
+                        
+                        async with message.channel.typing():
+                            await asyncio.sleep(1)
+                            await message.reply(response, mention_author=False)
 
 def setup(client):
     client.add_cog(ai(client))
