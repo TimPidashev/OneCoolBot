85fe126505aca93a8d71101af3fec28b64dd45df
timothypidashev <pidashev.tim@gmail.com>
2021-06-07 16:10:22 +0000

    cleanup help

--- cogs/commands/help.py
+++ cogs/commands/help.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
 from utils import data, embed, log
+from db import db
 import asyncio
 
 EXCLUDED_COMMANDS = ['help']
+db.connect("./data/database.db")
 
 class help(commands.Cog):
     def __init__(self, client):
@@ -11,14 +13,107 @@ class help(commands.Cog):
     
     async def on_ready(self):
         pass
-
+        
     @commands.command(aliases=["hlp", "h"])
     async def help(self, context, arg=None):
         await log.cog_command(self, context)
         if arg is None:
             async with context.typing():
                 await asyncio.sleep(1)
-                message = await context.reply(embed=await embed.help_page_1(context), mention_author=False)
+
+                #help
+                prefix = db.record("SELECT Prefix FROM guilds WHERE GuildID = ?",
+                    context.guild.id,
+                )[0]
+                
+                page_1 = discord.Embed(
+                    title="Index",
+                    description="The home page of the help command!", 
+                    colour=0x9b59b6
+                )
+                fields = [("`General`", "**The basic commands for day-to-day tasks.", False),
+                        ("`Economy`", "A global market and trading system, complete with its own currency!", False),
+                        ("`Games`", "Play with friends, compete with strangers, and make some extra coins while having fun!", False),
+                        ("`Music`", "Listen to low-latency music streams for studying and hanging with friends in voice-chat!", False),
+                        ("`Moderation`", "Make sure your server is always under control, with an advanced toolset for your moderators, and auto-moderation for the tech-savvy!", False),
+                        ("`Settings`", "Configure OneCoolBot with ease right in discord, with a dashboard coming later.", False)]
+
+                page_1.set_footer(
+                    text="To scroll through pages, react to the arrows below."
+                )
+
+                for name, value, inline in fields:
+                    page_1.add_field(name=name, value=value, inline=inline)
+
+                page_2 = discord.Embed(
+                    title="General", 
+                    description="The overview of the general commands.", 
+                    colour=0x9b59b6
+                )
+                fields = [("`help`", "Your looking at this command.\n**subcommands:** `aliases`", False),
+                        ("`info`", "Displays bot status, ping, and other miscellaneous content.\n**subcommands:** `aliases` `help`", False),
+                        ("`serverinfo`", "Displays server info, such as user count.\n**subcommands:** `aliases` `help`", False),
+                        ("`userinfo`", "Displays user info, discord stats, and the like.\n**subcommands:** `aliases` `help`", False)]
+
+                page_2.set_footer(
+                    text=f"Handy tip! To see what a command can do, use the `help` subcommand."
+                )
+
+                for name, value, inline in fields:
+                    page_2.add_field(name=name, value=value, inline=inline)
+
+                page_3 = discord.Embed(
+                    title="Economy", 
+                    description="A global market and trading system, complete with its own currency!", 
+                    colour=0x9b59b6
+                )
+                fields = [("Under Construction!", "Economy is currently being polished and completely refactored. Should be complete in a weeek or two.", False)]
+
+                for name, value, inline in fields:
+                    page_3.add_field(name=name, value=value, inline=inline)
+
+                page_4 = discord.Embed(
+                    title="Games", 
+                    description="Play with friends, compete with strangers, and make some extra coins all while having fun!", 
+                    colour=0x9b59b6
+                )
+                fields = [("Under Construction!", "Games are currently being polished and completely refactored. ETA not known yet, be patient!", False)]
+
+                for name, value, inline in fields:
+                    page_4.add_field(name=name, value=value, inline=inline)
+
+                page_5 = discord.Embed(
+                    title="Music",
+                    description="Listen to low-latency music streams for studying and hanging with friends in voice-chat!",
+                    colour=0x9b59b6
+                )
+                fields = [("Commands", "`connect` connect bot to voice chat\n`play` <search song to play>\n`pause` pause player\n`resume` resume player\n`skip` skip current song\n`stop`\n`volume` change volume\n`shuffle` shuffle queue\n`equalizer` change equalizer\n`queue` see songs queue\n`current` see currently played song\n`swap` swap song\n`music` see music status\n`spotify` see spotify rich presence", False)]
+                
+                for name, value, inline in fields:
+                    page_5.add_field(name=name, value=value, inline=inline)
+
+                page_5.set_footer(
+                    text=f"Music is stable, but will be refactored to use new discord features and spotify integration, including playlists!"
+                )
+
+                page_6 = discord.Embed(
+                    title="Moderation", 
+                    description="Make sure your server is always under control, with an advanced toolset for your moderators, and auto-moderation for the tech-savvy!", 
+                    colour=0x9b59b6
+                )
+                fields = [(f"`{prefix}clear` <message_amount>", "Clear messages from a channel.", False),
+                        (f"`{prefix}kick` <@member> <reason>", "Kick mentioned member from server.", False),
+                        (f"`{prefix}ban` <@member> <reason>", "Ban mentioned member from server.", False),
+                        (f"`{prefix}unban` <@member> <reason>", "Unbans mentioned member from server.", False)]
+
+                for name, value, inline in fields:
+                    page_6.add_field(name=name, value=value, inline=inline)
+
+                page_6.set_footer(
+                    text="Is being refactored, as is most of the bot lol. Will be complete and shiny with ai moderation soon!"
+                )
+                
+                message = await context.reply(embed=page_1, mention_author=False)
                 await message.add_reaction("◀️")
                 await message.add_reaction("▶️")
                 await message.add_reaction("❌")
@@ -36,46 +131,46 @@ class help(commands.Cog):
                             current_page += 1
 
                             if current_page == 2:
-                                await message.edit(embed=await embed.help_page_2(context))
+                                await message.edit(embed=page_2)
                                 await message.remove_reaction(reaction, user)
                             
                             elif current_page == 3:
-                                await message.edit(embed=await embed.help_page_3(context))
+                                await message.edit(embed=page_3)
                                 await message.remove_reaction(reaction, user)
 
                             elif current_page == 4:
-                                await message.edit(embed=await embed.help_page_4(context))
+                                await message.edit(embed=page_4)
                                 await message.remove_reaction(reaction, user)
 
                             elif current_page == 5:
-                                await message.edit(embed=await embed.help_page_5(context))
+                                await message.edit(embed=page_5)
                                 await message.remove_reaction(reaction, user)
 
                             elif current_page == 6:
-                                await message.edit(embed=await embed.help_page_6(context))
+                                await message.edit(embed=page_6)
                                 await message.remove_reaction(reaction, user)
                         
                         if str(reaction.emoji) == "◀️" and current_page > 1:
                             current_page -= 1
                             
                             if current_page == 1:
-                                await message.edit(embed=await embed.help_page_1(context))
+                                await message.edit(embed=page_1)
                                 await message.remove_reaction(reaction, user)
 
                             elif current_page == 2:
-                                await message.edit(embed=await embed.help_page_2(context))
+                                await message.edit(embed=page_2)
                                 await message.remove_reaction(reaction, user)
                             
                             elif current_page == 3:
-                                await message.edit(embed=await embed.help_page_3(context))
+                                await message.edit(embed=page_3)
                                 await message.remove_reaction(reaction, user)
 
                             elif current_page == 4:
-                                await message.edit(embed=await embed.help_page_4(context))
+                                await message.edit(embed=page_4)
                                 await message.remove_reaction(reaction, user)
 
                             elif current_page == 5:
-                                await message.edit(embed=await embed.   help_page_5(context))
+                                await message.edit(embed=page_5)
                                 await message.remove_reaction(reaction, user)
 
                         if str(reaction.emoji) == "❌":
@@ -98,6 +193,9 @@ class help(commands.Cog):
 
             else:
                 return
+
+        else:
+            return
 
 
     
