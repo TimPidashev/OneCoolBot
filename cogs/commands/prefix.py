9e75a6b93add3519778e448b80e8351c2b907f96
timothypidashev <pidashev.tim@gmail.com>
2021-06-07 16:15:46 +0000

    cleanup prefix

--- cogs/commands/prefix.py
+++ cogs/commands/prefix.py
@@ -1,7 +1,10 @@
 import discord
 import asyncio
 from discord.ext import commands
-from utils import data, log
+from utils import log
+from db import db
+
+db.connect("./data/database.db")
 
 class prefix(commands.Cog):
     def __init__(self, client):
@@ -13,7 +16,7 @@ class prefix(commands.Cog):
     @commands.group(pass_context=True, invoke_without_command=True, aliases=["prfx", "prf", "pr", "p"])
     async def prefix(self, context):
         await log.cog_command(self, context)
-        prefix = await data.get_prefix(context)
+        prefix = db.record(f"SELECT Prefix FROM guilds WHERE GuildID = {context.guild.id}")[0]
         await context.reply(f"The current prefix is `{prefix}`", mention_author=False)
 
     @prefix.command(aliases=["alias", "als", "a"])
