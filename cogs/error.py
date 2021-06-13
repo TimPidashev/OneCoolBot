165691c42b697729d4ccda2c2d3af48a3694a8ac
timothypidashev <pidashev.tim@gmail.com>
2021-06-13 04:10:40 +0000

    remove depracated and unused dependencies

--- cogs/error.py
+++ cogs/error.py
@@ -1,16 +1,4 @@
 import discord
-import traceback
-import wavelink
-import sys
-import aiohttp
-from discord.ext import commands
-from termcolor import colored, cprint
-from discord import Member, Embed
-from discord.utils import get
-import asyncio
-import sqlite3
-from db import db
-from utils import embed, log
 
 class error(commands.Cog):
     def __init__(self, client):
