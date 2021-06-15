aa6c50f395904b389d7a5358f96d42924e2a61a3
timothypidashev <pidashev.tim@gmail.com>
2021-06-15 18:09:47 +0000

    remove references to old db

--- commands/prefix.py
+++ commands/prefix.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from utils import log
 from db import db
 
-db.connect("./data/database.db")
+# db.connect("./data/database.db")
 
 class prefix(commands.Cog):
     def __init__(self, client):
@@ -13,21 +13,21 @@ class prefix(commands.Cog):
     async def on_ready(self):
         pass
 
-    @commands.group(pass_context=True, invoke_without_command=True, aliases=["prfx", "prf", "pr", "p"])
-    async def prefix(self, context):
-        await log.cog_command(self, context)
-        prefix = db.record(f"SELECT Prefix FROM guilds WHERE GuildID = {context.guild.id}")[0]
-        await context.reply(f"The current prefix is `{prefix}`", mention_author=False)
+    # @commands.group(pass_context=True, invoke_without_command=True, aliases=["prfx", "prf", "pr", "p"])
+    # async def prefix(self, context):
+    #     await log.cog_command(self, context)
+    #     prefix = db.record(f"SELECT Prefix FROM guilds WHERE GuildID = {context.guild.id}")[0]
+    #     await context.reply(f"The current prefix is `{prefix}`", mention_author=False)
 
-    @prefix.command(aliases=["alias", "als", "a"])
-    async def aliases(self, context):
-        await log.cog_command(self, context)
-        await context.reply("**prefix** aliases: `prfx` `prf` `pr` `p`", mention_author=False)
+    # @prefix.command(aliases=["alias", "als", "a"])
+    # async def aliases(self, context):
+    #     await log.cog_command(self, context)
+    #     await context.reply("**prefix** aliases: `prfx` `prf` `pr` `p`", mention_author=False)
 
-    @prefix.command(aliases=["hlp", "h"])
-    async def help(self, context):
-        await log.cog_command(self, context)
-        await context.reply(f"Shows server prefix.", mention_author=False)
+    # @prefix.command(aliases=["hlp", "h"])
+    # async def help(self, context):
+    #     await log.cog_command(self, context)
+    #     await context.reply(f"Shows server prefix.", mention_author=False)
 
 def setup(client):
     client.add_cog(prefix(client))
