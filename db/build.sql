d9b1bf589895b4f9f9c6544ac714bddc3e31418f
timothypidashev <pidashev.tim@gmail.com>
2021-06-19 04:09:56 +0000

    old sql structure back

--- db/build.sql
+++ db/build.sql
@@ -1,4 +1,32 @@
 CREATE TABLE IF NOT EXISTS guilds(
-    GuildID BIGINT PRIMARY KEY,
-    Prefix TEXT DEFAULT '.'
+    GuildID integer PRIMARY KEY,
+    Prefix text DEFAULT "."
 );
+
+CREATE TABLE IF NOT EXISTS guildconfig(
+    GuildID integer PRIMARY KEY,
+    Levels text DEFAULT "OFF",
+    LevelMessages text DEFAULT "OFF",
+    LevelMessage text DEFAULT "This is a level message!",
+    LevelMessageChannel integer DEFAULT 0,
+    UpdateMessage text DEFAULT "NONE",
+    Economy text DEFAULT "OFF",
+    Games text Default "OFF",
+    Music text DEFAULT "OFF",
+    BotAI text DEFAULT "OFF",
+    ErrorMessages text DEFAULT "On"
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    GuildID integer,
+    UserID integer,
+    XP integer DEFAULT 0,
+    Level integer DEFAULT 0,
+    Coins integer DEFAULT 0,
+    Stars integer DEFAULT 0,
+    GlobalMessageCount integer DEFAULT 0,
+    MessagesPerWeek text DEFAULT CURRENT_DATE,
+    XPLock text DEFAULT CURRENT_TIMESTAMP,
+    AILock text DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("GuildID", "UserID")
+)
