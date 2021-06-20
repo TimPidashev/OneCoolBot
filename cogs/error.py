b48dc372e0e51ba9639747955f6bfb615f568762
timothypidashev <pidashev.tim@gmail.com>
2021-06-20 04:57:19 +0000

    add command.CheckFailure

--- cogs/error.py
+++ cogs/error.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from utils import log
 
 class error(commands.Cog):
     def __init__(self, client):
@@ -8,6 +9,13 @@ class error(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         pass
-        
+
+    @commands.Cog.listener()
+    async def on_command_error(self, context, error):
+
+        if isinstance(error, commands.CheckFailure):
+            await context.reply(f"**oops :|**\nYou are not priveleged enough to use this command.", mention_author=False)
+            await log.is_owner_false(self, context, error)
+
 def setup(client):
     client.add_cog(error(client))
