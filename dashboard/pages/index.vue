2e9785ec8ba4a6fc803487dc9aeadd690dcba525
timothypidashev <pidashev.tim@gmail.com>
2021-06-23 19:25:53 +0000

    fix vue script error

--- dashboard/pages/index.vue
+++ dashboard/pages/index.vue
@@ -1,44 +1,32 @@
 <template>
-  <div :style="{ backgroundImage: `url(${image})`}">
-    <div class="container">
-      <div>
-        <logo />
-        <h1 class="title">
-          OneCoolBot
-        </h1>
-        <h2 class="subtitle">
-          An advanced and extremely capable cyberpunk discord bot that's out of this world! 
-        </h2>
-        <div class="links">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            class="button--green"
-          >
-            Documentation
-          </a>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            class="button--grey"
-          >
-            GitHub
-          </a>
-        </div>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        OneCoolBot
+      </h1>
+      <h2 class="subtitle">
+        An advanced and extremely capable cyberpunk discord bot that's out of this world! 
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import backgroundIMG from "assets/background.png";export default {
-  data() {
-    return {
-      image: backgroundIMG,    
-    };
-  }
-};
-</script>
 
 <style>
 .container {
