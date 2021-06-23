1b7f23c7368566dc53d59fa16aae4cc9e7786f2b
timothypidashev <pidashev.tim@gmail.com>
2021-06-23 19:13:29 +0000

    Remove defaults, add background script

--- dashboard/pages/index.vue
+++ dashboard/pages/index.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        OneCoolBot
-      </h1>
-      <h2 class="subtitle">
-        An advanced and extremely capable cyberpunk discord bot that's out of this world! 
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div :style="{ backgroundImage: `url(${image})`}">
+    <div class="container">
+      <div>
+        <logo />
+        <h1 class="title">
+          OneCoolBot
+        </h1>
+        <h2 class="subtitle">
+          An advanced and extremely capable cyberpunk discord bot that's out of this world! 
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green"
+          >
+            Documentation
+          </a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+import backgroundIMG from "assets/background.png";export default {
+  data() {
+    return {
+      image: backgroundIMG,    
+    };
   }
-}
+};
 </script>
 
 <style>
