d81f31eabecde6d38f295a5369c6d45398401162
timothypidashev <pidashev.tim@gmail.com>
2021-06-25 02:29:08 +0000

    add custom checks

---
+++ utils/checks.py
@@ -0,0 +1,13 @@
+import discord
+import asyncio
+import json
+from discord.ext import commands
+from db import db
+
+#loading bot config
+with open("config.json") as file:
+    config = json.load(file)
+
+#checks if invoked command is run by owner
+async def is_owner(context):
+    return context.message.author.id in config["owner_ids"]
