ff6fad939c446958714addd1071ca03b7465d2b6
timothypidashev <pidashev.tim@gmail.com>
2021-06-27 23:39:32 +0000

    final polish and finishing touches

--- commands/prefix.py
+++ commands/prefix.py
@@ -10,7 +10,7 @@ class prefix(commands.Cog):
 
     async def on_ready(self):
         pass
-#dynamic helper function per user???? Where user gets the help output if they are new to the command or have never used it before
+    #dynamic helper function per user???? Where user gets the help output if they are new to the command or have never used it before
     @commands.group(pass_context=True, invoke_without_command=True, aliases=["prfx", "prf", "pr", "p"])
     async def prefix(self, context):
         await log.cog_command(self, context)
@@ -22,28 +22,17 @@ class prefix(commands.Cog):
         await log.cog_command(self, context)
         embed = discord.Embed(colour=0x9b59b6)
 
-        fields=[("***Command:***", "`prefix`", True),
-                ("***Options:***", "`help` `aliases` `change`", True),
-                ("`change`: ***Usage:*** see `change help`", "Changes server prefix.", False),
-                ("`aliases`:", "Shows command aliases.", False),
-                ("`help`:", "Shows this message.", False)]
+        fields=[("***Command***", "`prefix`", True),
+                ("***Options***", "`change` `help`", True),
+                ("***Aliases***", "`prfx` `prf` `pr` `p`", True),
+                ("`change`", "Changes server prefix. See `change help`", False),
+                ("`help`", "Shows this message.", False)]
 
         embed.set_footer(text="Use this command to change my prefix!")
 
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)    
 
-        await context.reply(embed=embed, mention_author=False)
-
-    @prefix.command(aliases=["alias", "als", "a"])
-    async def aliases(self, context, arg=None):
-        await log.cog_command(self, context)
-        embed = discord.Embed(colour=0x9b59b6)
-        embed.add_field(
-            name="***Command:*** `prefix`",
-            value="***Aliases:*** `prfx` `prf` `pr` `p`",
-            inline=False
-        )
         await context.reply(embed=embed, mention_author=False)
 
     @prefix.command(aliases=["chng", "c"])
@@ -58,9 +47,11 @@ class prefix(commands.Cog):
         if arg == "help" or arg == "hlp" or arg == "h":
             embed = discord.Embed(colour=0x9b59b6)
             
-            fields = [("***Command:***", "`prefix change`", True),
-                      ("***Options:***", "`help`", True),
-                      ("`help`:", "Shows this message.", False)]
+            fields = [("***Command***", "*prefix* `change`", True),
+                      ("***Options***", "`prefix` `help`", True),
+                      ("***Aliases***", "`chng` `c`", True),
+                      ("`prefix`", "Type a new server prefix after `change`.", False),
+                      ("`help`", "Shows this message.", False)]
 
             for name, value, inline in fields:
                 embed.add_field(name=name, value=value, inline=inline)  
