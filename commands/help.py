49234f2838e69dcefb59be4be6f6503d513a6384
timothypidashev <pidashev.tim@gmail.com>
2021-07-02 03:16:07 +0000

    set help command to adhere to color themes

--- commands/help.py
+++ commands/help.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from utils import log
+from utils import colours, log
 from db import db
 import asyncio
 from dislash import *
@@ -24,7 +24,7 @@ class help(commands.Cog):
         page_1 = discord.Embed(
             title="Index",
             description="The home page of the help command!", 
-            colour=0x9b59b6
+            colour=await colours.colour(context)
         )
         fields = [("`General`", "**The basic commands for day-to-day tasks.", False),
                 ("`Economy`", "A global market and trading system, complete with its own currency!", False),
@@ -43,7 +43,7 @@ class help(commands.Cog):
         page_2 = discord.Embed(
             title="General", 
             description="The overview of the general commands.", 
-            colour=0x9b59b6
+            colour=await colours.colour(context)
         )
         fields = [("`help`", "Your looking at this command.\n**subcommands:** `aliases`", False),
                 ("`info`", "Displays bot status, ping, and other miscellaneous content.\n**subcommands:** `aliases` `help`", False),
@@ -60,7 +60,7 @@ class help(commands.Cog):
         page_3 = discord.Embed(
             title="Economy", 
             description="A global market and trading system, complete with its own currency!", 
-            colour=0x9b59b6
+            colour=await colours.colour(context)
         )
         fields = [("Under Construction!", "Economy is currently being polished and completely refactored. Should be complete in a weeek or two.", False)]
 
@@ -70,7 +70,7 @@ class help(commands.Cog):
         page_4 = discord.Embed(
             title="Games", 
             description="Play with friends, compete with strangers, and make some extra coins all while having fun!", 
-            colour=0x9b59b6
+            colour=await colours.colour(context)
         )
         fields = [("Under Construction!", "Games are currently being polished and completely refactored. ETA not known yet, be patient!", False)]
 
@@ -80,7 +80,7 @@ class help(commands.Cog):
         page_5 = discord.Embed(
             title="Music",
             description="Listen to low-latency music streams for studying and hanging with friends in voice-chat!",
-            colour=0x9b59b6
+            colour=await colours.colour(context)
         )
         fields = [("Commands", "`connect` connect bot to voice chat\n`play` <search song to play>\n`pause` pause player\n`resume` resume player\n`skip` skip current song\n`stop`\n`volume` change volume\n`shuffle` shuffle queue\n`equalizer` change equalizer\n`queue` see songs queue\n`current` see currently played song\n`swap` swap song\n`music` see music status\n`spotify` see spotify rich presence", False)]
         
@@ -94,7 +94,7 @@ class help(commands.Cog):
         page_6 = discord.Embed(
             title="Moderation", 
             description="Make sure your server is always under control, with an advanced toolset for your moderators, and auto-moderation for the tech-savvy!", 
-            colour=0x9b59b6
+            colour=await colours.colour(context)
         )
         fields = [(f"`{prefix}clear` <message_amount>", "Clear messages from a channel.", False),
                 (f"`{prefix}kick` <@member> <reason>", "Kick mentioned member from server.", False),
@@ -178,29 +178,6 @@ class help(commands.Cog):
             except asyncio.TimeoutError:
                 await message.delete()
                 break
-
-    @commands.command()
-    async def test(self, context):
-        message = await context.send(
-            "This message has a select menu!",
-            components=[
-                SelectMenu(
-                    custom_id="test",
-                    placeholder="Choose up to 2 options",
-                    max_values=2,
-                    options=[
-                        SelectOption("Option 1", "value 1"),
-                        SelectOption("Option 2", "value 2"),
-                        SelectOption("Option 3", "value 3")
-                    ]
-                )
-            ]
-        )
-        # Wait for someone to click on it
-        interaction = await message.wait_for_dropdown()
-        # Send what you received
-        labels = [option.label for option in interaction.select_menu.selected_options]
-        await interaction.reply(f"Options: {', '.join(labels)}")
 
 def setup(client):
     client.add_cog(help(client))
