bb77d5d22ef68c2f4c0fadfac658255ccd3f4150
Timothy Pidashev <pidashev.tim@gmail.com>
2021-07-27 06:32:50 -0700

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -11,7 +11,5 @@ import asyncio
 async def build_db():
     await db.build()
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(build_db())
-
+loop = asyncio.get_event_loop()
+loop.run_until_complete(build_db())
