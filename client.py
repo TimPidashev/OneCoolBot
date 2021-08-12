6441b3f006c1dab1719d632ff210a7844c883b3f
timothypidashev <pidashev.tim@gmail.com>
2021-08-12 23:15:01 +0000

    add build_database command

--- client.py
+++ client.py
@@ -229,6 +229,7 @@ async def restart(context):
     except Exception as error:
         await context.reply(f"**error :(**\n```diff\n- {error}```", mention_author=False)
 
+#maintenance
 @client.command(hidden=True, pass_context=True, aliases=["m"])
 @commands.check(checks.is_owner)
 async def maintenance(context, arg):
@@ -251,5 +252,17 @@ async def maintenance(context, arg):
     else:
         pass
 
+#build_db
+@client.command(hidden=True, pass_context=True, aliases=["bd"])
+@commands.check(checks.is_owner)
+async def build_database(context):
+    try:
+        await log.client_command(context)
+        await context.reply("Your wish is my command | Building database", mention_author=False)
+        await build_db()
+
+    except Exception as error:
+        await context.reply(f"**error :(**\n```diff\n- {error}```", mention_author=False)
+        
 client.loop.create_task(change_presence())
 client.run(Token, reconnect=True)
