43b14dc42ef84901436f3d8c535674660bcb0fc5
timothypidashev <pidashev.tim@gmail.com>
2021-08-12 22:57:56 +0000

    clean up checks

--- utils/checks.py
+++ utils/checks.py
@@ -12,10 +12,10 @@ import ez_db as db
 
 db = db.DB(db_path="./data/database/database.db", build_path="./data/database/build.sql")
 
+#owner_ids
 owner_id_str = db.record("SELECT OwnerIDS FROM botconfig")[0]
-
 owner_ids = map(int, owner_id_str.split(","))
 
 #checks if invoked command is run by owner
 async def is_owner(context):
-    return context.message.author.id in map(int, owner_id_str.split(","))      
+    return context.message.author.id in map(int, owner_id_str.split(","))
